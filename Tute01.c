ee4cbdac07fdc557508774ecbcf55fa0fd7a2732
IT22102546 <127377258+IT22102546@users.noreply.github.com>
2023-03-09 07:14:04 +0000

    Tute 1

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,23 @@
 
 #include <stdio.h>
 
-int main() {
-  
-  return 0;
-}
+int main()
+{
+  int mark1,mark2,avg,sum;
 
+  printf("Enter Marks1:");
+  scanf("%d",& mark1);
+
+  printf("Enter Mark2:");
+  scanf("%d",& mark2);
+
+  sum= mark1+ mark2;
+  avg=sum/2;
+
+  printf("Sum:%d\n",sum);
+  printf("Avarage:%d\n",avg);
+
+  return 0;
+
+  
+}
